5c34db0ebda4ed6dc69a66408090981adb729786
Tseng Shih Yang <iamdog43gmail.com>
2022-01-21 15:52:36 +0800

    完成訂票系統

---
+++ api/get_sessions.php
@@ -0,0 +1,10 @@
+<?php 
+include_once "../base.php";
+$movie=$Movie->find($_GET['id']);
+$date=$_GET['date'];
+
+
+
+for($i=1;$i<=5;$i++){
+    echo "<option value='$i'>{$ss[$i]} 剩餘座位 </option>";
+}
